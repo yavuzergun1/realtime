33854ec1f64b60eaa3db82195391a73c58a3cbb9
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-06-27 23:42:14 +0300

    socket api

--- client/src/Components/Palatte.js
+++ client/src/Components/Palatte.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { send } from "../socketApi";
 
 function Palatte() {
+  const [color, setColor] = useState("#000");
   return (
-    <div className='palette'>
-    <input type="color" />
-    <button>click</button>
+    <div className="palette">
+      <input
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <button onClick={() => send(color)}>click</button>
+      {color}
     </div>
-  )
+  );
 }
 
-export default Palatte
+export default Palatte;
--- client/src/socketApi.js
+++ client/src/socketApi.js
@@ -1,10 +1,14 @@
 import io from "socket.io-client";
 let socket;
-export const init= () => {
-    console.log('sunucuya bağlanılıyor...');
-  socket=io('http://localhost:3001',{
-    transports: ['websocket'],
+export const init = () => {
+  console.log("sunucuya bağlanılıyor...");
+  socket = io("http://localhost:3001", {
+    transports: ["websocket"],
   });
-  
-  socket.on('connect', ()=> console.log("bağlantı sağlandı"))
+
+  socket.on("connect", () => console.log("bağlantı sağlandı"));
+};
+
+export const send= (color) => {
+    socket.emit('newColor', color)
 }
